3706026af4c7fa9a4574c6e10c8ae6d6b40c80d1
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:40 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Homebrew tap for Akeyless CLI distribution";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ ruby bundler ];
+      };
+    });
+}
